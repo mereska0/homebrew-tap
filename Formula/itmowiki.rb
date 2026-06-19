28d1918e4fb02fa1caf7a2c1047a8079659c5fa6
Alexandr Byrgazov <504822@niuitmo.ru>
2026-06-19 16:02:18 +0300

    Update itmowiki to v0.1.4

--- Formula/itmowiki.rb
+++ Formula/itmowiki.rb
@@ -1,8 +1,8 @@
 class Itmowiki < Formula
   desc "ITMO wiki terminal client"
   homepage "https://github.com/mereska0/itmowiki"
-  url "https://github.com/mereska0/itmowiki/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "239ee8af26426de315b3fcb861deecf2181716296569886109ca46247a5869e6"
+  url "https://github.com/mereska0/itmowiki/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "go" => :build
